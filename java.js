b7ae52b8e59c3d65d63944fa6f95120c6da8a0ad
Chxwi961 <char.elhayek@gmail.com>
2025-11-05 13:31:56 -0500

    gave a label to all digit buttons (1-9, 0) using js logic. Note: When using Array.from(node list), its not a copy, its a reference to the actual thing. So if you do array[something] something it will actually change the thing in its origin. The more you know!

--- java.js
+++ java.js
@@ -15,3 +15,22 @@ for (let i=0 ; i < (cols * rows); i++) {
 
     buttoncontainer.appendChild(newCell)
 }
+
+let buttonArray = Array.from(buttoncontainer.children)
+let counter = 0; 
+
+for (let i=0 ; i<buttonArray.length; i++) {
+    if (i < 3) {
+        buttonArray[i].classList.add('clears')
+    } else if ([4,5,6,8,9,10,12,13,14,17].includes(i)) {
+        buttonArray[i].classList.add('digits')
+        counter += 1 
+        if (counter == 10) {
+            counter = 0
+        }
+        buttonArray[i].textContent = counter
+
+    } else {
+        buttonArray[i].classList.add('operations')
+    }
+}

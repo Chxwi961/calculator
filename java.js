189118742ef05709959687cfb13965cdfe93f401
Chxwi961 <char.elhayek@gmail.com>
2025-11-05 12:58:44 -0500

    Initial design of calculator, made a grid of buttons with perfect scaling with java. Will work on making the buttons look better

---
+++ java.js
@@ -0,0 +1,17 @@
+const buttoncontainer = document.querySelector(".buttoncontainer")
+const buttonHeight = buttoncontainer.offsetHeight
+const buttonWidth = buttoncontainer.offsetWidth
+
+let cols = 4
+let rows = 5
+
+for (let i=0 ; i < (cols * rows); i++) {
+    let newCell = document.createElement('div')
+    
+    newCell.style.width = `${buttonWidth / cols}px`
+    newCell.style.height = `${buttonHeight / rows}px`
+    newCell.style.backgroundColor = 'white'
+    newCell.classList.add('button')
+
+    buttoncontainer.appendChild(newCell)
+}
